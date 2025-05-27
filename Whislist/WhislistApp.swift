fed2e8f33b95060cfd07f702f1bb778c52c7ec0c
anshhoff <anshhoff@github.com>
2025-05-27 09:10:08 +0530

    Initial Commit

---
+++ Whislist/WhislistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhislistApp.swift
+//  Whislist
+//
+//  Created by Ansh Hardaha on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WhislistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
