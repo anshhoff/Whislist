9cd52b6d0592a7f1c5c0f127c8b2376c36a4ef0f
anshhoff <anshhoff@github.com>
2025-05-27 09:51:40 +0530

    made changes

--- Whislist/ContentView.swift
+++ Whislist/ContentView.swift
@@ -6,19 +6,46 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var wishes: [Wish]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(wishes) { wish in
+                    Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
+                }
+            }
+            .navigationTitle("WishList")
+            .overlay {
+                if wishes.isEmpty {
+                    ContentUnavailableView("My Wishlist", systemImage: "heart.circle", description: Text("No wishes yet. Add one to get Started"))
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
+#Preview("List With Sample data") {
+    let container = try! ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    container.mainContext.insert(Wish(title: "Master SwiftData"))
+    container.mainContext.insert(Wish(title: "Buy a new iPhone"))
+    container.mainContext.insert(Wish(title: "Practice Latin dances"))
+    container.mainContext.insert(Wish(title: "Travel to Europe"))
+    container.mainContext.insert(Wish(title: "Make a postive Impact"))
+
+    return ContentView()
+        .modelContainer(container)
+
+}
+
+#Preview("Empty List") {
     ContentView()
+        .modelContainer(for: Wish.self, inMemory: true)
 }
